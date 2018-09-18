c6e281dfb4b9810a0a4283400489dc33b8e944dc
Rasmus H. Hummelmose <rasmus@hummelmose.dk>
2018-09-18 08:56:33 +0200

    Broken

--- Vinni/Vinni.iOS/Main.cs
+++ Vinni/Vinni.iOS/Main.cs
@@ -10,7 +10,7 @@ namespace Vinni.iOS
     public class Application
     {
         // This is the main entry point of the application.
-        static void Main(string[] args)
+        static void Main(strings[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
