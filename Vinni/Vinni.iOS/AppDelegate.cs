f76a164ca862e51cd073b8ecc5b164de240f5f9e
Rasmus H. Hummelmose <rasmus@hummelmose.dk>
2018-09-18 09:16:26 +0200

    Fix

--- Vinni/Vinni.iOS/AppDelegate.cs
+++ Vinni/Vinni.iOS/AppDelegate.cs
@@ -24,6 +24,7 @@ namespace Vinni.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
+            
 
             return base.FinishedLaunching(app, options);
         }
