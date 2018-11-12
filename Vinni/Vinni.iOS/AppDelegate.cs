a922e55b991f9a48405417f68a2f94c134a5c397
Rasmus Hummelmose <rasmus@hummelmose.dk>
2018-11-12 13:01:53 +0100

    Triggering a change

--- Vinni/Vinni.iOS/AppDelegate.cs
+++ Vinni/Vinni.iOS/AppDelegate.cs
@@ -22,6 +22,7 @@ namespace Vinni.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            // Hi there!
             global::Xamarin.Forms.Forms.Init();
       LoadApplication(new App());
 
